db0d2e1afb6adedc82929e16c822d8278d6bb5c0
kanomiya <kanomiya@users.noreply.github.com>
2016-04-17 16:49:44 +0900

    レシピ追加、アイテム消費、MODID変更

--- src/main/java/com/kanomiya/mcmod/bomber/Bomber.java
+++ src/main/java/com/kanomiya/mcmod/bomber/Bomber.java
@@ -1,9 +1,15 @@
 package com.kanomiya.mcmod.bomber;
 
+import java.util.function.Consumer;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityEnderPearl;
 import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -11,6 +17,7 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -32,31 +39,51 @@ public class Bomber
 			@Override
 			public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand)
 			{
-				if (! worldIn.isRemote)
+				if (playerIn.capabilities.isCreativeMode || 0 < playerIn.inventory.clearMatchingItems(Item.getItemFromBlock(Blocks.TNT), 0, 1, null))
 				{
-					Entity entity = new EntityTNTPrimed(worldIn, playerIn.posX, playerIn.posY, playerIn.posZ, playerIn);
+					if (! worldIn.isRemote)
+					{
+						Entity entity = new EntityTNTPrimed(worldIn, playerIn.posX, playerIn.posY +playerIn.getEyeHeight(), playerIn.posZ, playerIn);
 
-					EntityEnderPearl dummyThrowable = new EntityEnderPearl(worldIn, playerIn);
-					dummyThrowable.setHeadingFromThrower(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, 1.5F, 1.0F);
+						EntityEnderPearl dummyThrowable = new EntityEnderPearl(worldIn, playerIn);
+						dummyThrowable.setHeadingFromThrower(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0f, 1.5f, 1.0f);
 
-					entity.motionX = dummyThrowable.motionX;
-					entity.motionY = dummyThrowable.motionY;
-					entity.motionZ = dummyThrowable.motionZ;
-					entity.rotationYaw = dummyThrowable.rotationYaw;
-					entity.rotationPitch = dummyThrowable.rotationPitch;
+						entity.motionX = dummyThrowable.motionX;
+						entity.motionY = dummyThrowable.motionY;
+						entity.motionZ = dummyThrowable.motionZ;
+						entity.rotationYaw = dummyThrowable.rotationYaw;
+						entity.rotationPitch = dummyThrowable.rotationPitch;
 
-					dummyThrowable = null;
+						dummyThrowable = null;
 
-					worldIn.spawnEntityInWorld(entity);
+						worldIn.spawnEntityInWorld(entity);
+
+						return new ActionResult(EnumActionResult.SUCCESS, itemStackIn);
+					}
 				}
 
 				return new ActionResult(EnumActionResult.PASS, itemStackIn);
 			}
 		}
+		.setCreativeTab(CreativeTabs.COMBAT)
 		.setRegistryName(new ResourceLocation(MODID, "itemTntLauncher"))
 		.setUnlocalizedName("itemTntLauncher");
 
 		GameRegistry.register(itemTntLauncher);
+
+		GameRegistry.addRecipe(new ItemStack(itemTntLauncher),
+					"IF",
+					"II",
+					'I', Items.IRON_INGOT,
+					'F', Items.FLINT_AND_STEEL
+				);
+
+		if (event.getSide().isClient())
+		{
+			Consumer<Item> simpleRegister = (item) -> ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+
+			simpleRegister.accept(itemTntLauncher);
+		}
 
 	}
 }
